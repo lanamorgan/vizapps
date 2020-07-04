d7c628c9b6c0419132529f55715cd52da16344f9
Lana <lana@cs.ucla.edu>
2020-07-04 16:59:08 -0700

    XML reports

--- icheck/queries/generate_queries.py
+++ icheck/queries/generate_queries.py
@@ -1,4 +1,4 @@
-import json, itertools
+import json, itertools, random
 import pandas as pd
 
 def gen(votes, pols, fname):
@@ -10,14 +10,18 @@ def gen(votes, pols, fname):
 	out.write(q1)
 	q2 = "SELECT pols.pid, pols.party, COUNT(CASE WHEN pol_votes.vote=person_votes.vote THEN 1 END) / CAST(COUNT(*) as FLOAT) AS agree_per FROM (SELECT * FROM icheck.dbo.pol_votes, icheck.dbo.votes WHERE vote_id = vid AND person_id = '{}' AND date BETWEEN '{}' AND '{}') AS person_votes, icheck.dbo.pol_votes pol_votes, icheck.dbo.pols pols WHERE pol_votes.vote_id=person_votes.vote_id AND pol_votes.person_id=pols.pid GROUP BY pols.pid, pols.party;\n"
 
-	for pair in itertools.product(dates, dates):
-		for pid in person_id:
+	small_people = random.sample(list(person_id), 8)
+	# small_dates = random.sample(list(dates), 11)
+
+	for pid in small_people:
+		small_dates = random.sample(list(dates), 8)
+		for pair in itertools.product(small_dates, small_dates):
 			mini, maxi = pair
 			out.write(q2.format(pid, mini, maxi))
 	out.close()
 
 def main():
-	output = "out/icheck_small.sql"
+	output = "out/icheck_tiny.sql"
 	dates = "../data/votes_small.csv"
 	pols = "../data/pols_small.csv"
 	gen(dates, pols, output)
